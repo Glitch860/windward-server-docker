028fc6d9636a0f3a7c14b11c886a337e77d2a91d
Aitor González Fernández <reimashi@gmail.com>
2018-01-06 18:38:16 +0100

    Ported binary download to Dockerfile

--- build.sh
+++ build.sh
@@ -3,15 +3,9 @@
 SERVER_REMOTE_FILE=http://www.tasharen.com/windward/WWServer.zip
 SERVER_TEMP_FILE=./WWServer.zip
 
-echo "Downloading latest dedicated server..."
-curl -O -J -L ${SERVER_REMOTE_FILE}
-unzip ${SERVER_TEMP_FILE}
-rm ${SERVER_TEMP_FILE}
-
 echo "Compiling to native code..."
 #mkbundle --static -o ./WWServer ./WWServer.exe --deps --cross default -z
 #rm ./WWServer.exe
 
 echo "Building docker image..."
 docker build . -t reimashi/windward:latest -t reimashi/windward:alpine-3.7
-rm ./WWServer.exe
